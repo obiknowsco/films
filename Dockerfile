9fd67fa3972e5c636b6029efe8b5497cf1dd7efc
Obi Knows <sam.nnodim@gmail.com>
2018-05-28 21:20:57 -0700

    builds, looks, good, TO THE BANANA BOAT!

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN npm run build
 
 FROM nginx
 COPY --from=builder /app/dist /usr/share/nginx/html
+EXPOSE 80
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,9 @@
     </small>
     <div class="tc mt3">
       <a href="/" title="Home" class="f6 dib ph2 link mid-gray dim">Home</a>
-      <a href="/about" title="About" class="dn f6 dib ph2 link mid-gray dim">About</a>
-      <a href="/terms"    title="Terms" class="dn f6 dib ph2 link mid-gray dim">Terms of Use</a>
-      <a href="/privacy"  title="Privacy" class="dn f6 dib ph2 link mid-gray dim">Privacy</a>
+      <!-- <a href="/about" title="About" class="f6 dib ph2 link mid-gray dim">About</a> -->
+      <!-- <a href="/terms" title="Terms" class="f6 dib ph2 link mid-gray dim">Terms of Use</a> -->
+      <!-- <a href="/privacy"  title="Privacy" class="f6 dib ph2 link mid-gray dim">Privacy</a> -->
     </div>
     <small class="advisory mt3 f6 db tc">
       <span class="dark-red">Note:</span> I do not own these films
